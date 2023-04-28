070099f3be906642427878677112e2c559a6cf73
abdrea454 <111761537+abdrea454@users.noreply.github.com>
2023-04-28 13:02:50 -0700

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,51 +1,50 @@
 #include "sort.h"
-
-void swap_nodes(listint_t **head, listint_t **n1, listint_t *n2);
-
 /**
- * insertion_sort_list - A function that sorts a doubly linked
- * list of integers in ascending order
- * @list: A pointer to the head of a doubly-linked list of integers
- * Description: Prints the list after each swap
- * Return: Nothing
- */
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort ailgorithm
+ * @list: pointer to the list head
+ * Return: no return
+ **/
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *prevoius;
+	listint_t *head_tmp1, *head_tmp2, *aux1, *aux2;
+	int flag;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
-
-	for (current = (*list)->next; current != NULL; current = current->next)
+	if (list)
 	{
-		prevoius = current->prev;
-		while (prevoius != NULL && current->n < prevoius->n)
+		head_tmp1 = *list;
+		head_tmp2 = *list;
+		while (list && head_tmp1->next)
 		{
-			swap_nodes(list, &prevoius, current);
-			print_list((const listint_t *)*list);
+			if (head_tmp1->next)
+			{
+				flag = 0;
+				head_tmp2 = head_tmp1;
+				while (head_tmp2 && head_tmp2->n > head_tmp2->next->n)
+				{
+					aux1 = head_tmp2;
+					aux2 = head_tmp2->next;
+					aux1->next = aux2->next;
+					if (aux2->next)
+						aux2->next->prev = aux1;
+					if (aux2)
+					{
+						aux2->prev = aux1->prev;
+						aux2->next = aux1;
+					}
+					if (aux1)
+						aux1->prev = aux2;
+					if (aux2->prev)
+						aux2->prev->next = aux2;
+					head_tmp2 = aux2->prev;
+					if (!aux2->prev)
+						*list = aux2;
+					print_list(*list);
+					flag = 1;
+				}
+			}
+			if (flag == 0)
+				head_tmp1 = head_tmp1->next;
 		}
 	}
-}
-
-/**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list
- * @head: A pointer to the head of the doubly-linked list
- * @n1: A pointer to the first node to swap
- * @n2: The second node to swap
- *
- * Return: Nothing
- */
-void swap_nodes(listint_t **head, listint_t **n1, listint_t *n2)
-{
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*head = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
 }
