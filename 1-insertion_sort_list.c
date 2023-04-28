6057f89980e5090ea3cd61909e83170b6670ab30
abdrea454 <111761537+abdrea454@users.noreply.github.com>
2023-04-28 08:21:07 -0700

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,49 +1,50 @@
 #include "sort.h"
 /**
- * insertion_sort_list - sort array with insertion method
- * @list: a duble linked list
- * Describtion: prints the list after each swap
- */
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort ailgorithm
+ * @list: pointer to the list head
+ * Return: no return
+ **/
 void insertion_sort_list(listint_t **list)
 {
-	listin_t *head, *left, *right, aux1, aux2;
+	listint_t *head_tmp1, *head_tmp2, *aux1, *aux2;
+	int flag;
 
-	if (list == NULL || *list == NULL)
-		return;
-	head = *list;
-	while (head != NULL)
-	{right = head, left = head->prev, aux1 = head;
-		while (aux1 && aux1->prev)
-		{right = aux1, left = aux1->prev;
-			if (left->n > right->n)
+	if (list)
+	{
+		head_tmp1 = *list;
+		head_tmp2 = *list;
+		while (list && head_tmp1->next)
+		{
+			if (head_tmp1->next)
 			{
-				if (right->next == NULL && left->prev == NULL)
+				flag = 0;
+				head_tmp2 = head_tmp1;
+				while (head_tmp2 && head_tmp2->n > head_tmp2->next->n)
 				{
-					left->next = NULL, right->prev = NULL;
-					right->next = left, left->prev = right, *list = right;
+					aux1 = head_tmp2;
+					aux2 = head_tmp2->next;
+					aux1->next = aux2->next;
+					if (aux2->next)
+						aux2->next->prev = aux1;
+					if (aux2)
+					{
+						aux2->prev = aux1->prev;
+						aux2->next = aux1;
+					}
+					if (aux1)
+						aux1->prev = aux2;
+					if (aux2->prev)
+						aux2->prev->next = aux2;
+					head_tmp2 = aux2->prev;
+					if (!aux2->prev)
+						*list = aux2;
+					print_list(*list);
+					flag = 1;
 				}
-				else if (right->next == NULL)
-				{
-					left->prev->next = right, right->prev =  left->prev;
-					left->next = NULL, right->next = lef, left->prev = right;
-				}
-				else if (left->prev == NULL)
-				{
-					right->prev = NULL, left->next = right->next, right->next->prev = left;
-					right->next = left, left->prev = right, *list = right;
-				}
-				else
-				{
-					left->prev->next = right, right->prev = left->prev;
-					left->next = right->next, right->next->prev = left;
-					right->next = left, left->prev = right;
-				}
-				print_list(*list), aux2 = left, left = right, right = aux2;
 			}
-			else
-				break;
-			aux1 = right, aux1 = aux1->prev;
+			if (flag == 0)
+				head_tmp1 = head_tmp1->next;
 		}
-		head = aux1, head = head->next;
 	}
 }
